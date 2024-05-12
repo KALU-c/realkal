b1313734336d885e1a98b676be8676d0e659db22
Endekalu Zemenu <endekaluzemenu2134@gmail.com>
2024-05-12 23:31:47 +0300

    11 or 12 ig

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
   return (
-    
+    <MaxWidthWrapper className="bg-red-500"><div>Hello World</div></MaxWidthWrapper>
   );
 }
